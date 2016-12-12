bf5e52912259e0b56c4ae4a16aaebbb4cb987f9d
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2016-12-12 17:41:16 +0100

    fix error in start-sample.sh

--- docs/start-sample.sh
+++ docs/start-sample.sh
@@ -9,30 +9,38 @@ organization_name="YOUR BLUEMIX ORGANIZATION"
 space_name="YOUR BLUEMIX SPACE"
 application_name="browserfied-ibmiotf-webapplication"
 
+pwd
 echo "--> 01. Install the npm packages for the webapplication"
+echo "**********************************************"
+echo ""
 cd ..
 cd public
 sudo npm install
-
+echo ""
+pwd
 echo "--> 02. Create browserified app.js file"
+echo "**********************************************"
+echo ""
 cd bin
 ./browserify.sh
-
 cd ..
+pwd
 cd ..
+pwd
+ls ..
 echo "--> 03. Push the application into Bluemix for following configuration:"
-
+echo "**********************************************"
 echo "a. User: '$user' API: '$bluemix_api'"
 echo "b. Organization: '$organization_name'"
 echo "c. Space: '$space_name'"
-
+echo ""
 echo "Insert your bluemix password:"
 # How to input a password in bash shell
 # http://stackoverflow.com/questions/3980668/how-to-get-a-password-from-a-shell-script-without-echoing
 read -s password
-cd ..
 cf login -a $bluemix_api -u $user -p $password -o $organization_name -s $space_name
-
+echo ""
+echo "**********************************************"
 echo "--> Starting push and log CF $application_name"
 cf spaces
 echo "****** show existing apps *********"
